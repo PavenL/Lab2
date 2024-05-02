fb613928172ac47a6d6ed017c428b808c1f8d412
Paven <paven.23@ichat.sp.edu.sg>
2024-05-02 10:53:08 +0800

    adding return values for cases

--- bmi.py
+++ bmi.py
@@ -8,11 +8,14 @@ def calculate_bmi(height,weight):
 
     if bmi<18.5:
         print("Under weight")
+        return -1
 
     if bmi<=18.5 or bmi <=25.0:
         print ("Normal weight")
+        return 0
 
     if bmi>25.0:
         print("Over weight")
+        return 1
 
 calculate_bmi(weight=85, height=1.73)
